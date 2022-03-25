d8b650b352804c325cf2b111b9f79b09ffcb4c36
Shashank Santosh Kapare <97380124+ammode@users.noreply.github.com>
2022-03-25 21:48:47 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,319 @@
+
+from playsound import playsound
+
+print("welcome to Kaun Banega Crorepati Game")
+print("Enter any Number from 1-10 to Start the Game : ")
+
+x = input("Enter any Number from 1-10 to start : ")
+if x == 1 :
+    print("Let's Start Kaun Banega Crorepati Game")
+
+   
+       
+
+#first question -->
+     
+print("Who is the Current Prime Minister of India ?")
+print("a = Narendra Modi")
+print("b = Rahul Gandhi")
+print("c = Sonia Gandhi")
+print("d = A P J Abdul Kalam")
+
+a = "Narendra Modi"
+b = "Rahul Gandhi"
+c = "Sonia Gandhi"
+d = "A P J Abdul Kalam"
+
+    
+s = input("Enter Your Answer")
+
+if s == a :
+    print("Yes , You are Right")
+    print("Congragulations !! Move to Next Question -->")
+    
+if s != a :
+    exit()
+    #next question -->2
+    
+print("Which of this ingredients is used to make Lassi ?")
+print("a = Lemon")
+print("b = Dahi")
+print("c = Ginger")
+print("d = Cocum")
+
+a = "Lemon"
+b = "Dahi"
+c = "Ginger"
+d = "Cocum"
+
+y = input("Enter your Answer :")
+
+if y == b :
+   print("Yes , You are right")
+   print("Congragulations !! Move to Next Question -->")
+   
+if y != b :
+    exit() 
+
+#next question 3-->
+
+print("Listen the Dialogue & Name the Actor ?")
+
+# music dilogue 
+
+x = input("Enter 2 to Listen the music")
+
+playsound('Pushpa Pushpa Raj.mp3')
+
+
+print("a = Allu Arjun")
+print("b = Shreyas Talpade")
+print("c = Aamir Khan")
+print("d = Akshay Kumar")
+
+
+
+a = "Allu Arjun"
+b = "Shreyas Talpade"
+c = "Aamir Khan"
+d = "Akshay Kumar"
+
+y = input("Enter your Answer :")
+
+if y == a :
+   print("Yes , You are right")  
+   print("Congragulations !! Move to Next Question -->")
+
+if y != a:
+    exit()
+#next question 4 --> 
+
+print("Which of this Letter is Used in Grid Game ?")
+print("a = A")
+print("b = S")
+print("c = N")
+print("d = X")
+
+a = "A"
+b = "S"
+c = "N"
+d = "X"
+
+y = input("Enter your Answer :")
+
+if y == d :
+   print("Yes , You are right")
+   print("Congragulations !! Move to Next Question -->")
+   
+if y != d :
+     exit()   
+
+#next question 5 -->
+   
+print("Which Actor is Named as Chulbul Pandey in his Movie ?")
+print("a = Salman Khan")
+print("b = Sharukh Khan")
+print("c = Akshay Kumar")
+print("d = Arjun Kapoor")
+
+a = "Salman Khan"
+b = "Sharukh Khan"
+c = "Akshay Kumar"
+d = "Arjun Kapoor"
+
+y = input("Enter your Answer :")
+
+if y == a :
+   print("Yes , You are right")
+   print("Congragulations !! Move to Next Question -->")
+   
+if y != a :
+     exit() 
+
+#next question 6--> 
+#INTERMEDIATE QUESTIONS STARTS -->
+
+print("First Round is Over !! Now It's Time for Second Round")
+
+
+print("The International Literacy Day is observed on ?")
+print("a = Sep 22")
+print("b = Nov 22")
+print("c = Sep 8")
+print("d = Oct 21")
+
+a = "Sep 22"
+b = "Nov 22"
+c = "Sep 8"
+d = "Oct 21"
+
+y = input("Enter your Answer :")
+
+if y == c :
+   print("Yes , You are right")
+   print("Congragulations !! Move to Next Question -->")
+   
+if y != c :
+    exit()    
+
+#next question 7-->
+
+print("Bahubali festival is related to ?")
+print("a = Hinduism")
+print("b = Buddhism") 
+print("c = Jainism")
+print("d = Islam")
+
+a = "Hinduism"
+b = "Buddhism"
+c = "Jainism"
+d = "Islam"
+
+y = input("Enter your Answer :")
+
+if y == c :
+   print("Yes , You are right")
+   print("Congragulations !! Move to Next Question -->")
+   
+if y != c :
+    exit()
+    
+#next question 8 -->
+
+print("What is the Chemical Formula of Water ?")
+print("a = H2O")
+print("b = NA2SO4") 
+print("c = H2SO4")
+print("d = H2O2")
+
+a = "H2O"
+b = "NA2SO4"
+c = "H2SO4"
+d = "H202"
+
+y = input("Enter your Answer :")
+
+if y == a :
+   print("Yes , You are right")
+   print("Congragulations !! Move to Next Question -->")
+   
+if y != a :
+    exit()
+
+#next question 9 -->
+
+print("Circle Style and Standard Style are two Varients of Which Sports ?")
+print("a = Kabbadi")
+print("b = Volleyball") 
+print("c = Kho-Kho")
+print("d = BasketBall")
+
+a = "Kabbadi"
+b = "Volleyball"
+c = "Kho-Kho"
+d = "BasketBall"
+
+y = input("Enter your Answer :")
+
+if y == a :
+   print("Yes , You are right")
+   print("Congragulations !! Move to Next Question -->")
+   
+if y != a :
+    exit()
+
+ 
+  #next question 10--> 
+
+
+print("Who was the First Player to Hit Six Sixes in Six Balls ?")
+print("a = MS.DHONI")
+print("b = VIRAT KOHLI") 
+print("c = YUVRAJ SINGH")
+print("d = VIRENDRA SEHWAG")
+
+a = "MS.DHONI"
+b = " VIRAT KOHLI"
+c = "YUVRAJ SINGH"
+d = "VIRENDRA SEHWAG"
+
+y = input("Enter your Answer :")
+
+if y == c :
+   print("Yes , You are right")
+   print("Congragulations !! Move to Next Question -->")
+   
+if y != c :
+    exit()
+    
+#next question 11 -->    
+
+print("What does .txt Means ?")
+print("a = The File is Text Message")
+print("b = The File is terminate xhaust transform") 
+print("c = The File is terminate xhaust transformation")
+print("d = The File is Blank Statement")
+
+a = "The File is Text Message"
+b = "The File is terminate xhaust transform"
+c = "The File is terminate xhaust transformation"
+d = "The File is Blank Statement"
+
+y = input("Enter your Answer :")
+
+if y == a :
+   print("Yes , You are right")
+   print("Congragulations !! Move to Next Question -->")
+   
+if y != a :
+    exit()
+    
+#next qustion --> 12
+print("CONGRAGULATIONS !! TO BE HERE ...... NOW LAST 2 QUESTIONS WILL BE TRICKY")
+
+print("Who Invented the First Stock Ticker in 1867 ?")
+print("a = Thomson Edison")
+print("b = Edward Calahan") 
+print("c = Robert Barclay")
+print("d = David Gestetner")
+
+a = "Thomson Edison"
+b = "Edward Calahan"
+c = "Robert Barclay"
+d = "David Gestetner"
+
+y = input("Enter your Answer :")
+
+if y == b :
+   print("Yes , You are right")
+   print("Congragulations !! Move to Next Question -->")
+   
+if y != b :
+    exit()
+    
+#next question --> 13 
+
+print("What is the name of the first person to be born in Antarctica ?")
+print("a = Emilio Palma")
+print("b = James Waddle") 
+print("c = Chars Vicki")
+print("d = James Wadley")
+
+a = "Emilio Palma"
+b = "James Waddle"
+c = "Chars Vicki"
+d = "James Wadley"
+
+y = input("Enter your Answer :")
+
+if y == a :
+   print("Yes , You are right")
+   print("Congragulations !! Move to Next Question -->")
+   
+if y != a :
+    exit()
+    
+print("CONGRAGULATIONS !! YOU WON THE KAUN BANEGA CROREPATI GAME")
+
+#Our Code Ends Here 
+#Total Lines 319
